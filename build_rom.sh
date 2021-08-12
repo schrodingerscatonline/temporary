b8e8ee92afd66d470bdd0e702e26ee641a5ed025
Mukul Sharma <87255315+Lucifer-morning-star-96@users.noreply.github.com>
2021-08-12 14:51:37 +0530

    Added few things

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Lucifer-morning-star-96/frostmanifest.git --depth 1 -b Lighthouse .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-                
+                  
 # build rom 
 . build/envsetup.sh
 lunch lighthouse_lavender-userdebug
