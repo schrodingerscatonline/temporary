20aecea1e57541a150e78c799043ded456c493c9
Razs <73821326+rasenss@users.noreply.github.com>
2021-08-17 13:20:59 +0700

    xdroid

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_ginkgo-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 make xd 
 
 
