b52bf036a2a8ccd4fd434d8004c086d968e72003
amier_san09 <73158213+MhmmdAmier@users.noreply.github.com>
2021-10-05 06:22:02 +0700

    initial build aex

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/MhmmdAmier/local_manifest.git --depth 1 -b bananadroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/MhmmdAmier/local_manifest.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch banana_ulysse-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
+lunch aosp_ulysse-userdebug
 export TZ=Asia/Jakarta #put before last build command
-make banana
+export KBUILD_BUILD_USER=amier_san09
+export KBUILD_BUILD_HOST=Gabuters-Dev
+m aex
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
