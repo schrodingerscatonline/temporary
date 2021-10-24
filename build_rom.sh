829502437c1439c31a037c5fc8a21a9de4f246e6
Razs <73821326+rasenss@users.noreply.github.com>
2021-10-24 12:01:01 +0700

    Fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rasenss/manifest_local.git --depth 1 -b Aosp11 .repo/local_manifests
+git clone https://github.com/rasenss/manifest_local.git --depth 1 -b Aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
